10447b42d1594bb76918a5dd36beadf467df9576
TeRaNozaVr1 <117114468+TeRaNozaVr1@users.noreply.github.com>
2025-02-28 17:41:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 from fastapi import FastAPI, HTTPException
 from solana.rpc.async_api import AsyncClient
 from solders.transaction import Transaction
-from solana.system_program import transfer
+from solders.system_program import transfer
 from solana.publickey import PublicKey
 from solders.signature import Signature
 from solders.keypair import Keypair
